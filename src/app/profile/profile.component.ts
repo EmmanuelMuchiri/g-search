e661139da636c2060487644e0d8f9673b352a907
chaloo56 <chaloo56@gmail.com>
2019-07-14 04:05:15 +0300

    made a few changes

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -7,7 +7,7 @@ import { ProfileService } from '../profile.service';
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
-  profile:any[];
+  profile:ProfileComponent[];
   repos: any[];
   username:string;
 
