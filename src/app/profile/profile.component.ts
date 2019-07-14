d2cc09103d970e3cec506488d9a6e92f8d3e3e03
chaloo56 <chaloo56@gmail.com>
2019-07-14 03:36:23 +0300

    made a few changes

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -20,7 +20,7 @@ export class ProfileComponent implements OnInit {
   	this.profileService.getProfileInfo().subscribe(profile => {
   		console.log(profile);
   		this.profile = profile;
-  	});
+  	})
 
   	this.profileService.getProfileRepos().subscribe(repos => {
   		console.log(repos);
